f20bca7b3d129fd430d0f1abf7f76fbfb47edda5
Chris Klosowski <cklosowski@gmail.com>
2014-03-27 07:56:55 -0700

    #3 adding in the libraries for Facebook integration
    This still has a way to go, I'll need to do a middleware app if I want to prevent the user from having to register their own Facebook app.

--- includes/libs/social-loader.php
+++ includes/libs/social-loader.php
@@ -2,3 +2,15 @@
 global $ppp_twitter_oauth;
 require_once( PPP_PATH . '/includes/libs/twitter.php');
 $ppp_twitter_oauth = new PPP_Twitter();
+
+global $ppp_facebook_oauth;
+require_once( 'facebook/facebook.php' );
+
+$config = array(
+	'appId' => '652453918135601',
+	'secret' => '3760af96dd3498761c307d0023bc446c',
+	'fileUpload' => false, // optional
+	'allowSignedRequest' => false, // optional, but should be set to false for non-canvas apps
+);
+
+$ppp_facebook_oauth = new Facebook($config);
