ca5b1b75a336f5e40b2af1552cc50ca763616fb9
Pippin Williamson <pippin@pippinsplugins.com>
2014-03-27 20:28:04 -0500

    Fix undefined index errors on settings

--- includes/admin/admin-pages.php
+++ includes/admin/admin-pages.php
@@ -43,7 +43,7 @@ function ppp_admin_page() {
 						<?php $post_types = get_post_types( array( 'public' => true, 'publicly_queryable' => true ), NULL, 'and' ); ?>
 						<?php if ( array_key_exists( 'attachment', $post_types ) ) { unset( $post_types['attachment'] ); } ?>
 						<?php foreach ( $post_types as $post_type => $type_data ): ?>
-							<input type="checkbox" name="ppp_options[post_types][<?php echo $post_type; ?>]" value="1" id="<?php echo $post_type; ?>" <?php checked( $ppp_options['post_types'][$post_type], '1', true ); ?> />&nbsp;<label for="<?php echo $post_type; ?>"><?php echo $type_data->labels->name; ?></label></br />
+							<input type="checkbox" name="ppp_options[post_types][<?php echo $post_type; ?>]" value="1" id="<?php echo $post_type; ?>" <?php checked( isset( $ppp_options['post_types'][$post_type] ), true, true ); ?> />&nbsp;<label for="<?php echo $post_type; ?>"><?php echo $type_data->labels->name; ?></label></br />
 						<?php endforeach; ?>
 					</td>
 				</tr>
@@ -51,7 +51,7 @@ function ppp_admin_page() {
 				<tr valign="top">
 					<th scope="row"><?php _e( 'Advanced', 'ppp-txt' ); ?><br /><span style="font-size: x-small;"><?php _e( 'Tools for troubleshooting and advanced usage', 'ppp-txt' ); ?></span></th>
 					<td>
-						<input type="checkbox" name="ppp_options[enable_debug]" "<?php checked( '1', $ppp_options['enable_debug'], true ); ?>" value="1" /> Enable Debug
+						<input type="checkbox" name="ppp_options[enable_debug]" "<?php checked( true, isset( $ppp_options['enable_debug'] ), true ); ?>" value="1" /> Enable Debug
 					</td>
 				</tr>
 
