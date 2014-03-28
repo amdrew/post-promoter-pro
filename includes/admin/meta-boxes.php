2a94f4303fe69a509b9b73bdb959383ff166837b
Pippin Williamson <pippin@pippinsplugins.com>
2014-03-27 20:25:21 -0500

    don't use a constant for text domain.

--- includes/admin/meta-boxes.php
+++ includes/admin/meta-boxes.php
@@ -22,34 +22,34 @@ function ppp_tweet_schedule_callback() {
 	$exclude_style = ( !empty( $ppp_post_exclude ) ) ? 'display: none;' : '';
 	$override_style = ( empty( $ppp_post_override ) ) ? 'display: none;' : '';
 	?>
-	<input type="checkbox" name="_ppp_post_exclude" id="_ppp_post_exclude" value="1" <?php checked( '1', $ppp_post_exclude, true ); ?> />&nbsp;<label for="_ppp_post_exclude"><?php _e( 'Do not schedule social media promotion for this post.', PPP_CORE_TEXT_DOMAIN ); ?></label>
+	<input type="checkbox" name="_ppp_post_exclude" id="_ppp_post_exclude" value="1" <?php checked( '1', $ppp_post_exclude, true ); ?> />&nbsp;<label for="_ppp_post_exclude"><?php _e( 'Do not schedule social media promotion for this post.', 'ppp-txt' ); ?></label>
 	<div style="<?php echo $exclude_style; ?>" id="ppp-post-override-wrap">
-		<input type="checkbox" name="_ppp_post_override" id="_ppp_post_override" value="1" <?php checked( '1', $ppp_post_override, true ); ?> />&nbsp;<label for="_ppp_post_override"><?php _e( 'Override Default Text and Times', PPP_CORE_TEXT_DOMAIN ); ?></label>
+		<input type="checkbox" name="_ppp_post_override" id="_ppp_post_override" value="1" <?php checked( '1', $ppp_post_override, true ); ?> />&nbsp;<label for="_ppp_post_override"><?php _e( 'Override Default Text and Times', 'ppp-txt' ); ?></label>
 		<div class="post-override-matrix" style="<?php echo $override_style; ?>">
-			<label for="day1"><?php _e( 'Day 1', PPP_CORE_TEXT_DOMAIN ); ?></label>&nbsp;
-			<input type="text" placeholder="<?php _e( 'Social Text', PPP_CORE_TEXT_DOMAIN ); ?>" id="day1" size="150" name="_ppp_post_override_data[day1][text]" <?php if ( isset( $ppp_post_override_data['day1']['text'] ) ) {?>value="<?php echo htmlspecialchars( $ppp_post_override_data['day1']['text'] ); ?>"<?php ;}?> />&nbsp;
-			<input id="day1" type="text" placeholder="<?php _e( 'Time', PPP_CORE_TEXT_DOMAIN ); ?>" name="_ppp_post_override_data[day1][time]" class="share-time-selector" value="<?php echo ( isset( $ppp_post_override_data['day1']['time'] ) ) ? $ppp_post_override_data['day1']['time'] : $ppp_options['times']['day1']; ?>" size="8" /><br />
+			<label for="day1"><?php _e( 'Day 1', 'ppp-txt' ); ?></label>&nbsp;
+			<input type="text" placeholder="<?php _e( 'Social Text', 'ppp-txt' ); ?>" id="day1" size="150" name="_ppp_post_override_data[day1][text]" <?php if ( isset( $ppp_post_override_data['day1']['text'] ) ) {?>value="<?php echo htmlspecialchars( $ppp_post_override_data['day1']['text'] ); ?>"<?php ;}?> />&nbsp;
+			<input id="day1" type="text" placeholder="<?php _e( 'Time', 'ppp-txt' ); ?>" name="_ppp_post_override_data[day1][time]" class="share-time-selector" value="<?php echo ( isset( $ppp_post_override_data['day1']['time'] ) ) ? $ppp_post_override_data['day1']['time'] : $ppp_options['times']['day1']; ?>" size="8" /><br />
 
-			<label for="day2"><?php _e( 'Day 2', PPP_CORE_TEXT_DOMAIN ); ?></label>&nbsp;
-			<input type="text" placeholder="<?php _e( 'Social Text', PPP_CORE_TEXT_DOMAIN ); ?>" id="day2" size="150" name="_ppp_post_override_data[day2][text]" <?php if ( isset( $ppp_post_override_data['day2']['text'] ) ) {?>value="<?php echo htmlspecialchars( $ppp_post_override_data['day2']['text'] ); ?>"<?php ;}?> />&nbsp;
-			<input id="day2" type="text" placeholder="<?php _e( 'Time', PPP_CORE_TEXT_DOMAIN ); ?>" name="_ppp_post_override_data[day2][time]" class="share-time-selector"  value="<?php echo ( isset( $ppp_post_override_data['day2']['time'] ) ) ? $ppp_post_override_data['day2']['time'] : $ppp_options['times']['day2']; ?>" size="8" /><br />
+			<label for="day2"><?php _e( 'Day 2', 'ppp-txt' ); ?></label>&nbsp;
+			<input type="text" placeholder="<?php _e( 'Social Text', 'ppp-txt' ); ?>" id="day2" size="150" name="_ppp_post_override_data[day2][text]" <?php if ( isset( $ppp_post_override_data['day2']['text'] ) ) {?>value="<?php echo htmlspecialchars( $ppp_post_override_data['day2']['text'] ); ?>"<?php ;}?> />&nbsp;
+			<input id="day2" type="text" placeholder="<?php _e( 'Time', 'ppp-txt' ); ?>" name="_ppp_post_override_data[day2][time]" class="share-time-selector"  value="<?php echo ( isset( $ppp_post_override_data['day2']['time'] ) ) ? $ppp_post_override_data['day2']['time'] : $ppp_options['times']['day2']; ?>" size="8" /><br />
 
-			<label for="day3"><?php _e( 'Day 3', PPP_CORE_TEXT_DOMAIN ); ?></label>&nbsp;
-			<input type="text" placeholder="<?php _e( 'Social Text', PPP_CORE_TEXT_DOMAIN ); ?>" id="day3" size="150" name="_ppp_post_override_data[day3][text]" <?php if ( isset( $ppp_post_override_data['day3']['text'] ) ) {?>value="<?php echo htmlspecialchars( $ppp_post_override_data['day3']['text'] ); ?>"<?php ;}?> />&nbsp;
-			<input id="day3" type="text" placeholder="<?php _e( 'Time', PPP_CORE_TEXT_DOMAIN ); ?>" name="_ppp_post_override_data[day3][time]" class="share-time-selector"  value="<?php echo ( isset( $ppp_post_override_data['day3']['time'] ) ) ? $ppp_post_override_data['day3']['time'] : $ppp_options['times']['day3']; ?>" size="8" /><br />
+			<label for="day3"><?php _e( 'Day 3', 'ppp-txt' ); ?></label>&nbsp;
+			<input type="text" placeholder="<?php _e( 'Social Text', 'ppp-txt' ); ?>" id="day3" size="150" name="_ppp_post_override_data[day3][text]" <?php if ( isset( $ppp_post_override_data['day3']['text'] ) ) {?>value="<?php echo htmlspecialchars( $ppp_post_override_data['day3']['text'] ); ?>"<?php ;}?> />&nbsp;
+			<input id="day3" type="text" placeholder="<?php _e( 'Time', 'ppp-txt' ); ?>" name="_ppp_post_override_data[day3][time]" class="share-time-selector"  value="<?php echo ( isset( $ppp_post_override_data['day3']['time'] ) ) ? $ppp_post_override_data['day3']['time'] : $ppp_options['times']['day3']; ?>" size="8" /><br />
 
-			<label for="day4"><?php _e( 'Day 4', PPP_CORE_TEXT_DOMAIN ); ?></label>&nbsp;
-			<input type="text" placeholder="<?php _e( 'Social Text', PPP_CORE_TEXT_DOMAIN ); ?>" id="day4" size="150" name="_ppp_post_override_data[day4][text]" <?php if ( isset( $ppp_post_override_data['day4']['text'] ) ) {?>value="<?php echo htmlspecialchars( $ppp_post_override_data['day4']['text'] ); ?>"<?php ;}?> />&nbsp;
-			<input id="day4" type="text" placeholder="<?php _e( 'Time', PPP_CORE_TEXT_DOMAIN ); ?>" name="_ppp_post_override_data[day4][time]" class="share-time-selector"  value="<?php echo ( isset( $ppp_post_override_data['day4']['time'] ) ) ? $ppp_post_override_data['day4']['time'] : $ppp_options['times']['day4']; ?>" size="8" /><br />
+			<label for="day4"><?php _e( 'Day 4', 'ppp-txt' ); ?></label>&nbsp;
+			<input type="text" placeholder="<?php _e( 'Social Text', 'ppp-txt' ); ?>" id="day4" size="150" name="_ppp_post_override_data[day4][text]" <?php if ( isset( $ppp_post_override_data['day4']['text'] ) ) {?>value="<?php echo htmlspecialchars( $ppp_post_override_data['day4']['text'] ); ?>"<?php ;}?> />&nbsp;
+			<input id="day4" type="text" placeholder="<?php _e( 'Time', 'ppp-txt' ); ?>" name="_ppp_post_override_data[day4][time]" class="share-time-selector"  value="<?php echo ( isset( $ppp_post_override_data['day4']['time'] ) ) ? $ppp_post_override_data['day4']['time'] : $ppp_options['times']['day4']; ?>" size="8" /><br />
 
-			<label for="day5"><?php _e( 'Day 5', PPP_CORE_TEXT_DOMAIN ); ?></label>&nbsp;
-			<input type="text" placeholder="<?php _e( 'Social Text', PPP_CORE_TEXT_DOMAIN ); ?>" id="day5" size="150" name="_ppp_post_override_data[day5][text]" <?php if ( isset( $ppp_post_override_data['day5']['text'] ) ) {?>value="<?php echo htmlspecialchars( $ppp_post_override_data['day5']['text'] ); ?>"<?php ;}?> />&nbsp;
-			<input id="day5" type="text" placeholder="<?php _e( 'Time', PPP_CORE_TEXT_DOMAIN ); ?>" name="_ppp_post_override_data[day5][time]" class="share-time-selector"  value="<?php echo ( isset( $ppp_post_override_data['day5']['time'] ) ) ? $ppp_post_override_data['day5']['time'] : $ppp_options['times']['day5']; ?>" size="8" /><br />
+			<label for="day5"><?php _e( 'Day 5', 'ppp-txt' ); ?></label>&nbsp;
+			<input type="text" placeholder="<?php _e( 'Social Text', 'ppp-txt' ); ?>" id="day5" size="150" name="_ppp_post_override_data[day5][text]" <?php if ( isset( $ppp_post_override_data['day5']['text'] ) ) {?>value="<?php echo htmlspecialchars( $ppp_post_override_data['day5']['text'] ); ?>"<?php ;}?> />&nbsp;
+			<input id="day5" type="text" placeholder="<?php _e( 'Time', 'ppp-txt' ); ?>" name="_ppp_post_override_data[day5][time]" class="share-time-selector"  value="<?php echo ( isset( $ppp_post_override_data['day5']['time'] ) ) ? $ppp_post_override_data['day5']['time'] : $ppp_options['times']['day5']; ?>" size="8" /><br />
 
-			<label for="day6"><?php _e( 'Day 6', PPP_CORE_TEXT_DOMAIN ); ?></label>&nbsp;
-			<input type="text" placeholder="<?php _e( 'Social Text', PPP_CORE_TEXT_DOMAIN ); ?>" id="day6" size="150" name="_ppp_post_override_data[day6][text]" <?php if ( isset( $ppp_post_override_data['day6']['text'] ) ) {?>value="<?php echo htmlspecialchars( $ppp_post_override_data['day6']['text'] ); ?>"<?php ;}?> />&nbsp;
-			<input id="day6" type="text" placeholder="<?php _e( 'Time', PPP_CORE_TEXT_DOMAIN ); ?>" name="_ppp_post_override_data[day6][time]" class="share-time-selector"  value="<?php echo ( isset( $ppp_post_override_data['day6']['time'] ) ) ? $ppp_post_override_data['day6']['time'] : $ppp_options['times']['day6']; ?>" size="8" /><br />
-			<p><?php _e( 'Do not include links in your text, this will be added automatically.', PPP_CORE_TEXT_DOMAIN ); ?></p>
+			<label for="day6"><?php _e( 'Day 6', 'ppp-txt' ); ?></label>&nbsp;
+			<input type="text" placeholder="<?php _e( 'Social Text', 'ppp-txt' ); ?>" id="day6" size="150" name="_ppp_post_override_data[day6][text]" <?php if ( isset( $ppp_post_override_data['day6']['text'] ) ) {?>value="<?php echo htmlspecialchars( $ppp_post_override_data['day6']['text'] ); ?>"<?php ;}?> />&nbsp;
+			<input id="day6" type="text" placeholder="<?php _e( 'Time', 'ppp-txt' ); ?>" name="_ppp_post_override_data[day6][time]" class="share-time-selector"  value="<?php echo ( isset( $ppp_post_override_data['day6']['time'] ) ) ? $ppp_post_override_data['day6']['time'] : $ppp_options['times']['day6']; ?>" size="8" /><br />
+			<p><?php _e( 'Do not include links in your text, this will be added automatically.', 'ppp-txt' ); ?></p>
 		</div>
 	</div>
 	<?php
