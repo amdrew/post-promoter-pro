37bf7c417ff2e43389d33b70d72a8b235f2d0cc4
Chris Klosowski <cklosowski@gmail.com>
2014-03-31 00:56:04 -0700

    #16 - Fixing some CSS issues
    Re-ordered the content, added a char counter, and hopefully prepped this for an easier time when adding per day restrictions.

--- includes/scripts/js/ppp_custom.js
+++ includes/scripts/js/ppp_custom.js
@@ -8,4 +8,18 @@
 	$('#_ppp_post_exclude').click( function() {
 		$('#ppp-post-override-wrap').toggle();
 	});
+
 })(jQuery);
+
+function PPPCountChar(val) {
+	var len = val.value.length;
+	var lengthField = jQuery(val).next('.ppp-text-length');
+	lengthField.text(len);
+	if (len < 100 ) {
+		lengthField.css('color', '#339933');
+	} else if ( len >= 100 && len < 117 ) {
+		lengthField.css('color', '#CC9933');
+	} else if ( len > 117 ) {
+		lengthField.css('color', '#FF3333');
+	}
+};
