311aba694892873623ffe9190563a018ec6a5a52
Chris Klosowski <cklosowski@gmail.com>
2014-05-03 11:57:49 -0700

    No caching of the call to the API, and double checking it's an object.

--- post-promoter-pro.php
+++ post-promoter-pro.php
@@ -3,14 +3,14 @@
 Plugin Name: Post Promoter Pro
 Plugin URI: http://postpromoterpro.com
 Description: Schedule the promotion of blog posts for the next 6 days, with no further work.
-Version: 0.3.2
+Version: 0.3.3
 Author: Post Promoter Pro
 Author URI: http://postpromoterpro.com
 License: GPLv2
 */
 
 define( 'PPP_PATH', plugin_dir_path( __FILE__ ) );
-define( 'PPP_VERSION', '0.3.2' );
+define( 'PPP_VERSION', '0.3.3' );
 define( 'PPP_FILE', plugin_basename( __FILE__ ) );
 define( 'PPP_URL', plugins_url( '/', PPP_FILE ) );
 
