43c6349211d65c7b5020293e9124cb071671e30f
Chris Klosowski <cklosowski@gmail.com>
2014-08-11 14:15:07 -0400

    2.0b2

--- post-promoter-pro.php
+++ post-promoter-pro.php
@@ -3,14 +3,14 @@
 Plugin Name: Post Promoter Pro
 Plugin URI: http://postpromoterpro.com
 Description: Schedule the promotion of blog posts for the next 6 days, with no further work.
-Version: 2.0b1
+Version: 2.0b2
 Author: Post Promoter Pro
 Author URI: http://postpromoterpro.com
 License: GPLv2
 */
 
 define( 'PPP_PATH', plugin_dir_path( __FILE__ ) );
-define( 'PPP_VERSION', '2.0b1' );
+define( 'PPP_VERSION', '2.0b2' );
 define( 'PPP_FILE', plugin_basename( __FILE__ ) );
 define( 'PPP_URL', plugins_url( '/', PPP_FILE ) );
 
