a58028f57717097388b67aee64ed6ad44f866482
Chris Klosowski <cklosowski@gmail.com>
2014-11-08 12:47:40 -0700

    #14 - Adding validation to get pages request

--- includes/libs/facebook.php
+++ includes/libs/facebook.php
@@ -130,11 +130,16 @@ if( !class_exists( 'PPP_Facebook' ) ) {
 
 				$all_pages = json_decode( wp_remote_retrieve_body( wp_remote_get( 'https://graph.facebook.com/me/accounts?access_token=' . $access_token ) ) );
 				$pages = array();
-				foreach ( $all_pages->data as $page ) {
-					if ( in_array( 'CREATE_CONTENT', $page->perms ) ) {
-						$pages[] = $page;
+				if ( !empty( $all_pages ) {
+					foreach ( $all_pages->data as $page ) {
+						if ( in_array( 'CREATE_CONTENT', $page->perms ) ) {
+							$pages[] = $page;
+						}
 					}
+				} else {
+					$pages = false;
 				}
+
 
 				$pages = (object) $pages;
 				$ppp_social_settings['facebook']->available_pages = $pages;
